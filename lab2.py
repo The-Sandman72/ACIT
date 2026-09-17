d8ba4ecff01b8ae3e6a19d85500b7fb3594a5c2f
The_Sandman72 <pavansandhu2008@outlook.com>
2026-09-17 15:54:36 -0800

    edits

--- lab2.py
+++ lab2.py
@@ -5,9 +5,9 @@ GB = 1073741824
 num_entries = int(input("Please enter the number of entries per second: "))
 entry_size = int(input("Please enter the average number of bytes per entry: "))
 
-kb_size = (num_entries * entry_size) / KB
-mb_size = (num_entries * entry_size) / MB
-gb_size = (num_entries * entry_size) / GB
+kb_size = (num_entries * entry_size) * KB
+mb_size = (num_entries * entry_size) * MB
+gb_size = (num_entries * entry_size) * GB
 
 print("Storage Estimates")
 print(f"Per minute: {kb_size}KB")
