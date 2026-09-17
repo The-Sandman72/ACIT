178b83b7f00cf671dd611f79b1533e5b1a967dc0
The_Sandman72 <pavansandhu2008@outlook.com>
2026-09-17 15:42:55 -0800

    modified

--- inclass1.py
+++ inclass1.py
@@ -1,27 +1,43 @@
+# Create a variable named x and store an integer (whole number) inside it
 x = 16
+
+# Create a variable named y and store a string (any characters between single or double quotes) inside it
 y = 15
+
+# Create a variable named z and store a float in it
 z = 7.5
 
+# Change the value stored in the x variable to a new string
+x = "6"
 
-x = 6
+# Change the value stored in the y variable to a new boolean
 y = True
+
+# Change the value store in the z variable to a different float
 z = 5.53
 
+
+# Print the value the user entered from the previous section
 user_input = input("Enter a number: ")
 print(user_input)
+
+# Print the (current) value of the variable x
 print(x)
+
+# Print the *type* (not the value itself) of the value stored in the y variable
 print(type(y))
 
+# Create two variables, one containing the string CIT, and another containing the string 1515
 class_name = "CIT"
 class_number = "1515"
+
+# Using the two variables and a hard-coded letter, print the word ACIT1515 to the terminal
 def class_details():
   print(f"A{class_name} {class_number}")
 
 class_details()
 
-assignment_number = "week 2"
+assignment_number = "In-Class #1"
 print(f"Assignment: {assignment_number} created")
-
-
 
 
