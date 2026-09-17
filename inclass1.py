e2823ba132d9b11714db7031d666fda16d36b26d
The_Sandman72 <pavansandhu2008@outlook.com>
2026-09-17 15:31:58 -0800

    new file

---
+++ inclass1.py
@@ -0,0 +1,27 @@
+x = 16
+y = 15
+z = 7.5
+
+
+x = 6
+y = True
+z = 5.53
+
+user_input = input("Enter a number: ")
+print(user_input)
+print(x)
+print(type(y))
+
+class_name = "CIT"
+class_number = "1515"
+def class_details():
+  print(f"A{class_name} {class_number}")
+
+class_details()
+
+assignment_number = "week 2"
+print(f"Assignment: {assignment_number} created")
+
+
+
+
